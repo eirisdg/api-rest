580ad880539fdf11945b14fff86e07d22a59b02f
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-08-22 22:04:20 +0200

    Creando métodos get

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 'use strict'
 
+// Importado de librerías
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Inicio de la app
 const app = express()
 const port = process.env.PORT || 3000
 
+// Configuración de bodyParser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+
+// ENDPOINTS
+// Petición a la url "hola"
+// app.get('/hola', (req, res) => {
+//     res.send({message: 'Hola mundo!'})
+// })
+// // Petición con parámetros
+// app.get('/hola/:name', (req, res) => {
+//     res.send({message: `Hola ${req.params.name}!`})
+// })
+
+app.get('/api/product', (req, res) => {
+
+})
+
+app.get('', function(req, res) {
+
+})
+
+// INICIO DE SERVIDOR
 app.listen(port, () => {
     console.log(`Servidor corriendo sobre el puerto ${port}`);
 })
