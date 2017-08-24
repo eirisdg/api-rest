15a1dac1660b8c95124b750f8216a09d150cc519
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-08-24 22:00:13 +0200

    Mongo y Mongoose
    Instalado mongo y mongoose.
    Creadas varias peticiones

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Importado de librerías
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Inicio de la app
 const app = express()
@@ -24,14 +25,36 @@ app.use(bodyParser.json())
 // })
 
 app.get('/api/product', (req, res) => {
+    res.status(200).send({products: []})
+})
+
+app.get('/api/product/:productId', (req, res) => {
 
 })
 
-app.get('', function(req, res) {
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: 'El producto se ha recibido.'})
+})
+
+app.put('/put/product/:productId', (req, res) => {
 
 })
 
-// INICIO DE SERVIDOR
-app.listen(port, () => {
-    console.log(`Servidor corriendo sobre el puerto ${port}`);
+app.delete('/put/product/:productId', (req, res) => {
+
+})
+
+
+// Conexión a base de datos e inicio de servidor
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+    if (err) {
+        return console.log(`Error al conectar a la base de datos ${err}`)
+    }
+    console.log('Conexión a la base de datos establecida')
+
+    // INICIO DE SERVIDOR
+    app.listen(port, () => {
+        console.log(`Servidor corriendo sobre el puerto ${port}`);
+    })
 })
